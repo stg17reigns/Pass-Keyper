a13cf0b511565ea2d0cf2df2b82038799f76a1cb
saurabh pal <saurabh170399@gmail.com>
2021-11-23 14:23:07 +0530

    main

--- pass_keyper/lib/Models/box_model.dart
+++ pass_keyper/lib/Models/box_model.dart
@@ -1,6 +1,5 @@
 import 'package:hive/hive.dart';
 import 'package:pass_keyper/Models/password_model.dart';
-import 'package:pass_keyper/Pages/group_page.dart';
 
 class Boxes {
   static Box<PassWordManager> getAccounts() =>
